d1dc32810370dd1b126634308ad4da09685de99f
Heather Roantree <n8870349@qut.edu.au>
2023-08-05 23:54:22 +1000

    Created basic plugin list table

--- Prototype/Front-End-Current/src/modules/mst/main.js
+++ Prototype/Front-End-Current/src/modules/mst/main.js
@@ -1,14 +1,14 @@
 import Upload from "./Upload";
 import SubMenu from "Components/SubMenu";
-import test from "./test";
+import PluginList from "./PluginList";
 
 //Define Pages to pass to Sub Navigations
 const navItems = [{
+    name: "Plugins",
+    component: PluginList
+},{
     name: "Upload Module",
     component: Upload
-},{
-    name: "Test",
-    component: test
 }];
 
 
