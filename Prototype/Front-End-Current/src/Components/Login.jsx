7c04786e2136b97f7e9bf4d526a47e3013e7d349
HandsomeMustache <Douglassbeard@gmail.com>
2023-08-07 12:17:49 +1000

    added fetch calls

--- Prototype/Front-End-Current/src/Components/Login.jsx
+++ Prototype/Front-End-Current/src/Components/Login.jsx
@@ -4,20 +4,50 @@ export default function Login(props) {
     const [register, setRegister] = useState(false);
 
     const handleLogin= (e) => {
-        props.setLoggedIn(true);
+        const form = document.getElementById("inputs");
+        const formData = new FormData(form);
+
+        fetch("http://localhost:5000/user/login", {
+            method: "POST",
+            body: formData,
+        }).then(response => (response.json()
+        )).then((response) => {
+            if (response.Success == true) {
+                    props.setLoggedIn(true);
+                }
+            console.log(response);
+            }
+        ).catch(function (error) {
+             console.log(error);
+        })
     }
 
     const handleRegister = (e) => {
-        props.setRegister(false);
+        const form = document.getElementById("inputs");
+        const formData = new FormData(form);
+        
+        fetch("http://localhost:5000/user/register", {
+            method: "POST",
+            body: formData,
+        }).then(response => (response.json()
+        )).then((response) => {
+            if (response.Success == true) {
+                    setRegister(false);
+                }
+            console.log(response);
+            }
+        ).catch(function (error) {
+             console.log(error);
+        })
     } 
 
     return(
         <div>
-            <form>
+            <form id="inputs">
                 <label>Username</label>
-                <input></input>
+                <input name="user"></input>
                 <label>Password</label>
-                <input></input>
+                <input name="password"></input>
             </form>
             {(register == false)?
             <button onClick={handleLogin}>Login</button>
