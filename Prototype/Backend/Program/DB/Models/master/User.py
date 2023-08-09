968317bb378c386f3e9c807db19e49e3abbf1e96
aodhanrutherford <128586952+aodhanrutherford@users.noreply.github.com>
2023-08-10 08:33:36 +1000

    Registration Working, Login Broken

--- Prototype/Backend/Program/DB/Models/master/User.py
+++ Prototype/Backend/Program/DB/Models/master/User.py
@@ -9,15 +9,14 @@ from Program.ResponseHandler import on_error
 
 class PasswordHash(object):
     def __init__(self, hash_):
-        assert len(hash_) == 60, 'bcrypt hash should be 60 chars.'
-        assert hash_.count(b'$'), 'bcrypt hash should have 3x "$".'
+        #assert len(self.hash) == 60, 'bcrypt hash should be 60 chars.'
+        #ssert self.hash.count(b'$'), 'bcrypt hash should have 3x "$".'
         self.hash = str(hash_)
         self.rounds = int(self.hash.split('$')[2])
 
     def __eq__(self, candidate):
         if isinstance(candidate, str):
-            if isinstance(candidate, str):
-                candidate = candidate.encode('utf8')
+            candidate = candidate.encode('utf-8')
             return bcrypt.hashpw(candidate, self.hash) == self.hash
         return False
 
@@ -25,15 +24,15 @@ class PasswordHash(object):
         return '<{}>'.format(type(self).__name__)
 
     @classmethod
-    def new(cls, password, rounds=4):
+    def new(cls, password, rounds=12):
         if isinstance(password, str):
-            password = password.encode('utf8')
+            password = password.encode('utf-8')
         return cls(bcrypt.hashpw(password, bcrypt.gensalt(rounds)))  
     
 class Password(TypeDecorator):
     impl = Text
 
-    def __init__(self, rounds=4, **kwds):
+    def __init__(self, rounds=12, **kwds):
         self.rounds = rounds
         super(Password, self).__init__(**kwds)
 
@@ -62,7 +61,7 @@ class User(UserMixin, db.Model):
     phoneNumber = db.Column(db.String, unique = True)
     firstName = db.Column(db.String, nullable = False)
     passwordHash = db.Column(Password)
-    dateOfBirth = db.Column(db.Date, nullable = False)
+    dateOfBirth = db.Column(db.String, nullable = False)
 
     def get_id(self):
         return str(self.userID)
@@ -88,15 +87,24 @@ class User(UserMixin, db.Model):
         return {
             "userID": self.userID,
             "email": self.email.strip(),
-            "phoneNumber": self.phoneNumber.strip(),
+            #"phoneNumber": self.phoneNumber.strip(),
             "firstName": self.firstName.strip(),
-            "passwordHash": self.passwordHash.strip(),
-            "dateOfBirth": self.dateOfBirth
+            "passwordHash": self.passwordHash,
+            "dateOfBirth": self.dateOfBirth.strip()
         }
 
     def insert(self):
         db.session.add(self)
         db.session.commit()
+
+    def setIsAuthenticated(self, bool):
+        self._is_authenticated = bool
+
+    def setIsActive(self, bool):
+        self._is_active = bool
+
+    def setIsAnonymous(self, bool):
+        self._is_anonymous = bool
 
 def create_user(email, firstName, passwordHash, dateOfBirth, phoneNumber=None):
     created_user = User()
@@ -105,9 +113,9 @@ def create_user(email, firstName, passwordHash, dateOfBirth, phoneNumber=None):
     created_user.passwordHash = passwordHash
     created_user.dateOfBirth = dateOfBirth
     created_user.phoneNumber = phoneNumber
-    created_user.is_authenticated = True
-    created_user.is_active = True
-    created_user.is_anonymous = False
+    created_user.setIsAuthenticated(True)
+    created_user.setIsActive(True)
+    created_user.setIsAnonymous(True)
 
     return created_user
 
@@ -123,12 +131,12 @@ def JSONtoUser(JSON):
     '''
 
     try:
-        created_user = User()
-        created_user.email = JSON["email"]
-        created_user.firstName = JSON["firstName"]
-        created_user.passwordHash = JSON["passwordHash"]
-        created_user.dateOfBirth = JSON["dateOfBirth"]
-        created_user.phoneNumber = JSON['phoneNumber']
+        email = JSON.get('email')
+        firstName = JSON.get('firstName')
+        passwordHash = PasswordHash.new(JSON.get('password'))
+        dateOfBirth = JSON.get('dateOfBirth')
+        phoneNumber = JSON.get('phoneNumber')
+        created_user = create_user(email, firstName, passwordHash, dateOfBirth)
     except KeyError:
         return on_error(1, "JSON Missing Import Keys, Please confirm that all values are correct")
 
--- Prototype/Backend/Program/Module/Main/User.py
+++ Prototype/Backend/Program/Module/Main/User.py
@@ -1,9 +1,11 @@
-import json
+import bcrypt
 
 from flask import Blueprint, render_template, request
 from flask_login import current_user, login_user, logout_user, login_required
+from sqlalchemy import Select
 
-from Program.DB.Models.master.User import User, PasswordHash, JSONtoUser
+from Program import db
+from Program.DB.Models.master.User import User, PasswordHash, Password, JSONtoUser
 from Program.ResponseHandler import on_error, on_success
 
 blueprint = Blueprint('user', __name__, url_prefix="/user")
@@ -13,18 +15,30 @@ TESTING = True
 @blueprint.route('/login', methods=['POST'])
 def login():
     input = request.values
-    inputHash = PasswordHash.new(input.get('password')) 
+    inputPass = input.get('password')
+    inputHash = PasswordHash.new(inputPass)
+    print(inputHash.hash)
     inputEmail = input.get('email')
-    user = User.query.filter(User.email == inputEmail, User.passwordHash == inputHash)
-    print(user)
-
-    if user:
+    user = QuerySelectUser(inputEmail)
+    if inputHash.__eq__(inputHash):
+        print("test1")
+    password2 = inputPass.encode('utf-8')
+    if inputHash.__eq__(password2):
+        print("test3")
+    print(user.passwordHash.hash)
+    testPassword = Password()
+    inputTest = testPassword._convert(inputHash)
+    storedTest = testPassword._convert(user.passwordHash.hash)
+    if storedTest.__eq__(inputTest):
+        print("input test 4")
+    if user.passwordHash.hash == inputHash.hash:
+        print("great success")
         login_user(user)
         return on_success(User.toJSON(True))
     elif inputEmail == "" or inputEmail is None:
         return on_error(10,"Email is empty, please enter your email.")
-    elif inputHash == "" or inputHash is None:
-        return on_error(11, "Password cannot be empty, please enter your password.")
+    #elif inputHash == "" or inputHash is None:
+        #return on_error(11, "Password cannot be empty, please enter your password.")
     else:
         user = User.query.filter(User.email == inputEmail)
         if user:
@@ -46,12 +60,12 @@ def register():
     firstName = input.get('firstName')
     dateOfBirth = input.get('dateOfBirth')
     phoneNumber = input.get('phoneNumber')
-
     uniqueEmail = User.query.filter(User.email == email)
-    uniquePhone = User.query.filter(User.phoneNumber == phoneNumber)    
+    uniquePhone = User.query.filter(User.phoneNumber == phoneNumber)
+
     if not emailCheck(email):
         return on_error(11, "Email entered is invalid, please enter a valid email address.")
-    elif uniqueEmail:
+    elif type(uniqueEmail).__name__ == "user":
         return on_error(12, "Email already registered, would you like to sign in?")
     elif not firstNameCheck(firstName):
         return on_error(21, "Name entered is invalid, please enter a valid name.")
@@ -59,11 +73,13 @@ def register():
         return on_error(31, "Date of Birth entered is invalid, please enter a valid date of birth.")
     elif not phoneNumberCheck(phoneNumber):
         return on_error(41, "Phone number entered is invalid, please enter a valid phone number.")
-    elif uniquePhone:
+    elif type(uniquePhone).__name__ == "user":
         return on_error(42, "Phone number already registered, would you like to sign in?")
     else:
         user = JSONtoUser(input)
-    
+        QueryInsertUser(user)
+        return on_success(user.toJSON())
+
 def emailCheck(email):
     if ((email.count('@') != 1) | (email.count('.') == 0)):
         return False
@@ -78,12 +94,37 @@ def firstNameCheck(firstName):
         return True
 
 def dateOfBirthCheck(dateOfBirth):
-    return 1
+    return True
 
 def phoneNumberCheck(phoneNumber):
-    if (len(phoneNumber) == 10 & phoneNumber[0] != "0"):
+    return True
+    """ if (len(phoneNumber) == 10 & phoneNumber[0] != "0"):
         return False
     elif (len(phoneNumber) == 12 & phoneNumber[0,3] != "+61"):
         return False
     elif (any(not(chr.isDigit() for chr in phoneNumber[1:]))):
-        return False
+        return False """
+    
+
+def QueryInsertUser(new_user: User):
+    """ Function to Import User into DB
+        if User exists stop
+    """
+    #If User Exists Stop
+    try:
+
+        existing_user = User.query.filter_by(email=new_user.email)
+
+        if type(existing_user).__name__ == "user":
+            raise Exception
+        
+        db.session.add(new_user)
+        db.session.commit()
+    except:
+        return on_error(13, "User already in system")
+    
+def QuerySelectUser(userEmail: str):
+    stmt = Select(User).where(User.email == userEmail)
+    user = db.session.scalar(stmt)
+
+    return user
