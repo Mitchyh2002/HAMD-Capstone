18ea3ff76605bdfa5cb170503d830aef562ae700
Mitchell Hosier <m.hosier@theyirosshop.com.au>
2023-08-28 19:22:13 +1000

    Added Setup File to Setup DB to System, Building block for Complex Content At Later Date.

---
+++ Prototype/Front-End-Current/src/Components/Breadcrumbs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import '../App.css';
+
+export default function Breadcrumbs({ prefix, moduleName }) {
+    const location = useLocation();
+    
+    let currentLink = '';
+
+    const crumbs = location.pathname.split('/')
+        .filter(crumb => crumb !== '')
+        .map(crumb => {
+            currentLink += `/${crumb}` 
+            crumb = crumb.replace("%20", " ");
+            crumb = crumb.charAt(0).toUpperCase() + crumb.slice(1);
+            crumb = crumb.replace(prefix, moduleName);
+            return(
+                <div className="crumb" key={crumb}>
+                    <Link to={currentLink}>{crumb}</Link>
+                </div>
+            )
+        })
+    
+    return(
+        <div className="breadcrumbs">
+            {crumbs}
+        </div>
+    )
+};
