c916ce12c3736b0f158e044992ce4975bc2957bf
Mitchyh2002 <nubletmitch@outlook.com>
2023-09-20 18:29:56 +1000

    Changes to base Setup to allow for base user login. :)

--- Prototype/Backend/create_db.py
+++ Prototype/Backend/create_db.py
@@ -2,6 +2,7 @@ from Program.DB.Builder import create_db
 from Program.DB.Models.grp.Groups import create_group
 from Program import init_app
 from Program.DB.Models.mst.Module import Module, create_module
+from Program.DB.Models.mst.moduleAccess import moduleAccess, create_moduleAccess
 from Program.Module.mst.Module import QueryInsertModule
 from Program.DB.Models.mst.User import PasswordHash, create_user
 from Program.DB.Models.mst.ModuleSecurity import init_masterPages
@@ -17,10 +18,11 @@ def sys_create():
     client = app.test_client()
 
 
-    new_module = create_module("mst", "Master Module", PasswordHash.new("M_STER@aaa").hash, True, '')
+    new_module = create_module("mst", "Platform Administration", PasswordHash.new("M_STER@aaa").hash, True, '')
     grp_module = create_module("grp", "Group management", PasswordHash.new("GroupMDL").hash, False, '')
-    user = create_user('sysAdmin@BeeAware.com', 'SYSAdmin', "@SySadmin!", '2000', None, 7)
-    
+    user = create_user('sysAdmin@BeeAware.com', 'SYSAdmin', "@SySadmin!", '2000', None, 9)
+    user.confirmed = True
+    userAccess = create_moduleAccess(user.userID, 'mst')
     grp = create_group('Default')
     with app.app_context():
         new_module.insert()
@@ -28,6 +30,7 @@ def sys_create():
         grp.insert()
         user.insert()
         init_masterPages()
+        userAccess.insert()
 
 if __name__ == "__main__":
     sys_create()
