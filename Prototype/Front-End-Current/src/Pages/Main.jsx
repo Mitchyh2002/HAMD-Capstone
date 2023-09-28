e6e66cef590578700ba1f9efd837bca9a305ba22
aodhanrutherford <aodhanrutherford@gmail.com>
2023-09-28 18:36:11 +1000

    Landing Page to Content

--- Prototype/Front-End-Current/src/Pages/Main.jsx
+++ Prototype/Front-End-Current/src/Pages/Main.jsx
@@ -3,17 +3,21 @@ import Header from "Components/Header";
 import LandingPage from "./LandingPage";
 import Content from "Content"
 import { Outlet } from "react-router-dom"
+import { useState } from "react";
 
 export default function Main(props) {
     const accountButton = "Account";
-    const logoutButton = "Logout"
+    const logoutButton = "Logout";
+    const [moduleClick, setModuleClick] = useState();
 
-    return(
+    return(<> { moduleClick ?
         <div style={{display: 'flex', flexDirection: 'column', height: "100vh"}}>
             <Header breadcrumbs = {<Breadcrumbs prefix="Mst" moduleName="Admin"/>} accountButton={accountButton} logoutButton={logoutButton}/>
             <Content modules={props.modules}/>
-        </div>
-    )
+        </div> :
+        <LandingPage modules={props.modules} setModuleClick={setModuleClick}/>
+    }
+    </>)
 }
 
 /* <LandingPage modules={props.modules}/> <Content modules={props.modules}/> */
