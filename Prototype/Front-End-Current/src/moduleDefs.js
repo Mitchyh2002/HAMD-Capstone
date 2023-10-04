00470f90aa221dbfc716bbae5cb6161e371bb33d
Mitchyh2002 <nubletmitch@outlook.com>
2023-10-04 20:08:34 +1000

    Created Dummy Front-End for GRP module

--- Prototype/Front-End-Current/src/moduleDefs.js
+++ Prototype/Front-End-Current/src/moduleDefs.js
@@ -1,13 +1,17 @@
 import mst_master from "./modules/mst/main.js";
 import { mst_pages } from "./modules/mst/main.js";
+import { grp_Main } from "./modules/grp/main.js";
+
     //IMPORT_END
 
 //Defintion of components, correlates the module id of the database to the component
 export const Modules = {
-    mst: mst_master
+    mst: mst_master,
+    grp: grp_Main
    }
 
 export const Directory = {
-  mst: mst_pages
+  mst: mst_pages,
+  grp: []
 }
 
