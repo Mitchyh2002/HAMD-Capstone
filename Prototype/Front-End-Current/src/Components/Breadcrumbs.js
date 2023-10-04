6c810e57466e6180e1f7e03e867334b8794160fd
HandsomeMustache <Douglassbeard@gmail.com>
2023-10-05 02:26:39 +1100

    fixed bread crumbs
    Bread crumbs now determine what display name to show based on server and using url path

--- Prototype/Front-End-Current/src/Components/Breadcrumbs.js
+++ Prototype/Front-End-Current/src/Components/Breadcrumbs.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import '../App.css';
+import { Directory } from "moduleDefs";
 
 /* Displays a breadcrumb link in the Header. 
 Takes in the module prefix and module name. */
 
-export default function Breadcrumbs({ prefix, moduleName }) {
+export default function Breadcrumbs(props) {
     const location = useLocation();
     
     let currentLink = '';
+    let currentModule;
 
     const crumbs = location.pathname.split('/')
         .filter(crumb => crumb !== '')
         .map(crumb => {
-            currentLink += `/${crumb}` 
-            crumb = crumb.replace("%20", " ");
-            crumb = crumb.replace("%20", " ");
-            crumb = crumb.replace("%20", " ");
-            crumb = crumb.charAt(0).toUpperCase() + crumb.slice(1);
-            crumb = crumb.replace(prefix, moduleName);
+            crumb = decodeURI(crumb);
+            currentLink += `/${crumb}`
+            const module = props.modules.find(obj => obj.prefix == crumb)
+            if(module){
+                crumb = module.displayName;
+                currentModule = module;
+            }
+
+            if (currentModule){
+                const page = Directory[currentModule.prefix].find(obj => obj.path == crumb);
+                if(page){
+                    crumb = currentModule.pages.find(obj => obj.pageCode == page.pageCode).pageName;
+                }
+            }
             return(
                 <div className="crumb" key={crumb}>
                     <Link to={currentLink}>{crumb}</Link>
